45a4287e9f99b85603a81ac01e4e98b438cedffe
Peter Lee <lephuocthach@gmail.com>
2023-03-30 14:28:40 +0700

    Update test (#2)

--- java-libs/apache-mina/src/test/java/SshdClientTest.java
+++ java-libs/apache-mina/src/test/java/SshdClientTest.java
@@ -1,16 +1,30 @@
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.apache.sshd.common.io.nio2.Nio2ServiceFactoryFactory;
+import org.apache.sshd.server.SshServer;
+import org.apache.sshd.server.keyprovider.SimpleGeneratorHostKeyProvider;
+import org.apache.sshd.server.shell.ProcessShellCommandFactory;
+import org.apache.sshd.server.shell.ProcessShellFactory;
 import org.junit.jupiter.api.Test;
 
 import peter.lee.client.SshdClient;
+import peter.lee.server.LoggingPasswordAuthenticator;
 
 class SshdClientTest {
     SshdClient client = new SshdClient();
 
     @Test
     void getFileList() throws Exception {
-        final String files = client.getFileList("localhost", 2224, "thachle", "password", 100);
-        assertThat(files).isNotEmpty()
-                         .contains("build.gradle", "settings.gradle");
+        try (SshServer sshServer = SshServer.setUpDefaultServer()) {
+            sshServer.setKeyPairProvider(new SimpleGeneratorHostKeyProvider());
+            sshServer.setPasswordAuthenticator(new LoggingPasswordAuthenticator());
+            sshServer.setCommandFactory(new ProcessShellCommandFactory());
+            sshServer.setShellFactory(new ProcessShellFactory("/bin/sh -i -l", "/bin/sh", "-i", "-l"));
+            sshServer.setIoServiceFactoryFactory(new Nio2ServiceFactoryFactory());
+            sshServer.start();
+            final String files = client.getFileList("localhost", sshServer.getPort(), "thachle", "password", 100);
+            assertThat(files).isNotEmpty()
+                             .contains("build.gradle", "README.md");
+        }
     }
 }
