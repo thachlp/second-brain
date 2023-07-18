7398307f21361d28324ab55844278f71c6b2db17
Thach Le <thach.le@linecorp.com>
2023-07-18 17:16:06 +0700

    Add springdoc for swagger

--- projects/coffee-shop/src/main/java/coffee/shop/SpringbootCoffeeShopApplication.java
+++ projects/coffee-shop/src/main/java/coffee/shop/SpringbootCoffeeShopApplication.java
@@ -1,11 +1,42 @@
 package coffee.shop;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
 
 @SpringBootApplication
+@Slf4j
 public class SpringbootCoffeeShopApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringbootCoffeeShopApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+            log.info("Let's inspect the beans provided by Spring Boot:");
+            final String[] beanNames = ctx.getBeanDefinitionNames();
+            Arrays.sort(beanNames);
+            for (String beanName : beanNames) {
+                log.info(beanName);
+            }
+        };
+    }
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(new Info().title("Coffee Shop API").version("1.0.0")
+                        .license(new License().name("MIT License").url("https://github.com/thachlp/second-brain/blob/main/LICENSE")));
     }
 }
