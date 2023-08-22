008f28eec63b7a8a6ee7516da4c7dda5f020fa65
Thach Le <thach.le@linecorp.com>
2023-08-22 21:45:07 +0700

    user-register

--- projects/coffee-shop/script/init_schema.sql
+++ projects/coffee-shop/script/init_schema.sql
@@ -42,3 +42,15 @@ CREATE TABLE order_detail (
     PRIMARY KEY (order_detail_id),
     FOREIGN KEY (order_id) REFERENCES `order`(order_id)
 );
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE user (
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    full_name VARCHAR(100),
+    phone_number VARCHAR(15),
+    address VARCHAR(255),
+    role ENUM('CUSTOMER', 'ADMIN', 'STAFF') NOT NULL DEFAULT 'CUSTOMER'
+);
