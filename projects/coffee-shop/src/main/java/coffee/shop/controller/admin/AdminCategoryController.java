28540eb30c5a76086568bbdb9e21f8d92d0c333b
Thach Le <thach.le@linecorp.com>
2023-08-26 16:07:12 +0700

    update/add-description-swagger-doc

--- projects/coffee-shop/src/main/java/coffee/shop/controller/admin/AdminCategoryController.java
+++ projects/coffee-shop/src/main/java/coffee/shop/controller/admin/AdminCategoryController.java
@@ -3,6 +3,8 @@ package coffee.shop.controller.admin;
 import coffee.shop.model.request.CategoryRequest;
 import coffee.shop.model.response.CommonDataResponse;
 import coffee.shop.service.CategoryService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,25 +15,28 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+@Tag(name = "AdminCategory", description = "Operations for admin managing categories")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/admin/categories")
 public class AdminCategoryController {
     private final CategoryService categoryService;
 
+    @Operation(summary = "Add a new category")
     @PostMapping
     ResponseEntity<CommonDataResponse> newCategory(@RequestBody CategoryRequest categoryRequest) {
         return new ResponseEntity<>(categoryService.addCategory(categoryRequest),
                 HttpStatus.CREATED);
     }
 
+    @Operation(summary = "Update a category")
     @PutMapping("/{id}")
     ResponseEntity<CommonDataResponse> updateCategory(@PathVariable("id") Long categoryId,
                                                       @RequestBody CategoryRequest categoryRequest) {
         return ResponseEntity.ok(categoryService.updateCategory(categoryId, categoryRequest));
     }
 
+    @Operation(summary = "Delete a category")
     @DeleteMapping("/{id}")
     ResponseEntity<CommonDataResponse> deleteCategory(@PathVariable("id") Long categoryId) {
         return ResponseEntity.ok(categoryService.deleteCategory(categoryId));
