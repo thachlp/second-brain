82d6bed7796585d7fb58eabc5456623e4594cf22
Thach Le <lephuocthach@gmail.com>
2023-08-26 16:08:43 +0700

    update/add-description-swagger-doc (#51)

--- projects/coffee-shop/src/main/java/coffee/shop/controller/user/UserInfoController.java
+++ projects/coffee-shop/src/main/java/coffee/shop/controller/user/UserInfoController.java
@@ -1,9 +1,10 @@
 package coffee.shop.controller.user;
 
 import coffee.shop.dto.request.UserRegistrationRequest;
-import coffee.shop.dto.response.UserRegistrationResponse;
 import coffee.shop.model.response.CommonDataResponse;
 import coffee.shop.service.UserInfoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,12 +12,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "UserInfo", description = "Operations for user registering, updating, retrieving info")
 @RestController
 @RequestMapping("/user")
 @RequiredArgsConstructor
 public class UserInfoController {
     private final UserInfoService userInfoService;
 
+    @Operation(summary = "Register a new user")
     @PostMapping("/register")
     public ResponseEntity<CommonDataResponse> registerUserAccount(@RequestBody UserRegistrationRequest userRegistrationDto) {
         final CommonDataResponse registeredResponse = userInfoService.registerNewUserAccount(userRegistrationDto);
